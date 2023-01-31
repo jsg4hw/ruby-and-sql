f352498006c7f4d16aec1cf8f1f3e96b2485ed4f
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-01-31 20:26:31 +0000

    created final tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_201603) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_202538) do
+  create_table "activities", force: :cascade do |t|
+    t.integer "salesperson_id"
+    t.integer "contact_id"
+    t.string "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -25,6 +33,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_201603) do
     t.string "last_name"
     t.string "email"
     t.integer "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
