0fb665fef8719ba6864a3fd44a521e320f68f6a8
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-01-31 21:57:32 +0000

    finished labs 2-models

--- labs/2-models.rb
+++ labs/2-models.rb
@@ -22,12 +22,32 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Lucho"
+new_salesperson["last_name"] = "Cerro"
+new_salesperson["email"] = "lucho@lucho.com"
+new_salesperson.save
+
+new_salesperson = Salesperson.new
+new_salesperson["first_name"] = "Sarah"
+new_salesperson["last_name"] = "Rogers"
+new_salesperson["email"] = "sarah@lucho.com"
+new_salesperson.save
+
 # 3. write code to display how many salespeople rows are in the database
 
-# ---------------------------------
+puts "Salespeople: #{Salesperson.all.count}"
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+
+sarah = Salesperson.find_by({"first_name" => "Sarah"})
+# puts sarah.inspect 
+sarah["email"] = "sarah@sarah.com"
+sarah.save
+# puts sarah.inspect
+
+# lucho 
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
@@ -36,3 +56,16 @@ Salesperson.destroy_all
 # Salespeople: 2
 # Ben Block
 # Brian Eng
+
+# puts "#{sarah.first_name} #{sarah.last_name}"
+all_salespeople = Salesperson.all
+# puts all_salespeople.inspect  #is an array
+
+for person in all_salespeople
+    # Salesperson.all.count
+    first_name = person["first_name"]
+    last_name = person["last_name"]
+    email = person["email"]
+    puts "#{first_name} #{last_name} (#{email})"
+
+end
